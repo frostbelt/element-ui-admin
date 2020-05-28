f26ae0d7c702902e01d96aaee15c5859dc05a5ac
caowenbin <caowenbin@360.cn>
2020-05-28 17:07:36 +0800

    init

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import { ElementUiAdminSample } from '@/entry';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    ElementUiAdminSample,
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <element-ui-admin-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,2 @@
+/* eslint-disable import/prefer-default-export */
+export { default as ElementUiAdminSample } from './element-ui-admin-sample.vue';
